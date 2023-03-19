a52c6151eac4ee2a0135adf598e9b8be85a73295
Olubukola Abiona <bukyabiona@gmail.com>
2023-03-19 03:00:27 -0300

    modified the editor logo

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,19 +1,22 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+// We are creating a new database named 'editor' which will be using version 1 of the database.
+  openDB('editor', 1, {
+     // Add our database schema if it has not already been initialized.
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('editor')) {
+        console.log('editor database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+            // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
+      db.createObjectStore('editor', { keyPath: 'id', autoIncrement: true });
+      console.log('editor database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {console.error('putDb not implemented')
+export const putDb = async (id, content) => {
 console.log('PUT to the database');
 const editorDb = await openDB('editor', 1);
 const tx = editorDb.transaction('editor', 'readwrite');
@@ -25,8 +28,10 @@ console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {console.error('getDb not implemented');
-  // Create a connection to the database database and version we want to use.
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Create a connection to the database and version we want to use.
   const editorDb = await openDB('editor', 1);
 
   // Create a new transaction and specify the database and data privileges.
